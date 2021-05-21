ce56645b6e7336073e5c4d17863f4dcc6280d088
your_username <abellouch34@gmail.com>
2021-05-21 16:09:13 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    return render_template("game.html")
+
+
+app.run(port=6001, debug=True)
